c44f28bce40d9420ec62c0f52c2c2846f5012f89
Shrayas <shrayasr@gmail.com>
2015-01-18 14:14:18 +0530

    Fixing tests

--- test/address_book/core/address_book_tests.clj
+++ test/address_book/core/address_book_tests.clj
@@ -2,17 +2,23 @@
   (:use midje.sweet)
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [address-book.core.handler :refer :all]))
+            [address-book.core.handler :refer :all]
+            [address-book.core.routes.address-book-routes :refer [contacts]]))
 
 (facts "Example GET and POST tests"
 
        (fact "Test GET"
              (let [response (app (mock/request :get "/"))]
                (:status response) => 200
-               (:body response) => (contains "Example GET")))
+               (:body response) => (contains "<div class=\"column-1\">A</div>")
+               (:body response) => (contains "<div class=\"column-1\">B</div>")
+               (:body response) => (contains "<div class=\"column-1\">C</div>")))
 
        (fact "Test POST"
              (let [response (app (mock/request :post "/post" 
-                                               {:example-post "Some data"}))]
-               (:status response) => 200
-               (:body response) => "You posted: Some data")))
+                                               {:name "D" 
+                                                :phone "4" 
+                                                :email "d@x.com"}))
+                   new-contact (filter #(= 4 (:id %)) @contacts)]
+               (:status response) => 302
+               (:name (first new-contact)) => "D")))
