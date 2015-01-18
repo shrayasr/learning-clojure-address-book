0c06895cf3f6a21aade3ece5385015995ef09de8
Shrayas <shrayasr@gmail.com>
2015-01-18 23:03:32 +0530

    Replacing the atom with actual db calls

--- src/address_book/core/routes/address_book_routes.clj
+++ src/address_book/core/routes/address_book_routes.clj
@@ -4,29 +4,21 @@
             [address-book.core.views.address-book-layout :refer 
              [common-layout 
               read-contact
-              add-contact-form]]))
-
-(def contacts (atom [{:id 1 :name "A" :phone "1" :email "a@x.com"}
-                     {:id 2 :name "B" :phone "2" :email "b@x.com"}
-                     {:id 3 :name "C" :phone "3" :email "c@x.com"}]))
-
-(defn next-id []
-  (->>
-    @contacts
-    (map :id)
-    (apply max)
-    (+ 1)))
+              add-contact-form]]
+            [address-book.core.models.query-defs :as query]))
 
 (defn post-route [request]
   (let [name (get-in request [:params :name])
         phone (get-in request [:params :phone])
         email (get-in request [:params :email])]
-    (swap! contacts conj {:id (next-id) :name name :phone phone :email email})
+    (query/insert-contact<! {:name name
+                             :phone phone
+                             :email email})
     (response/redirect "/")))
 
 (defn get-route [request]
   (common-layout
-    (for [contact @contacts]
+    (for [contact (query/all-contacts)]
       (read-contact contact))
     (add-contact-form)))
 
